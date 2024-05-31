3dbe499926be8fc0a0899c7793c21f3c93b5f47a
Fabian Margreiter <fabian.margreiter@gmail.com>
2024-05-31 15:10:17 +0200

    Added benchmarking functionality for parallel implementation

---
+++ 11/ex2/job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Execute job in the partition "lva" unless you have special requirements.
+#SBATCH --partition=lva
+# Name your job to be able to identify it later
+#SBATCH --job-name example
+# Maximum number of tasks (=processes) to start in total
+#SBATCH --ntasks=1
+# Maximum number of tasks (=processes) to start per node
+#SBATCH --ntasks-per-node=1
+# Enforce exclusive node allocation, do not share with other jobs
+#SBATCH --exclusive
+
+# Enable timing measurement by program
+touch timer.flag
+
+# Set number of threads
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+./$1
