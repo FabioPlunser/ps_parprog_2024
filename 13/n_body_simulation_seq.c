9a4ab09b51fa29713a9a2fda5b989b38544fe625
Dominik Barbist <d.barbist@hotmail.com>
2024-06-14 14:38:42 +0200

    chore: Update number of particles and time steps in n_body_simulation_seq.c

--- 13/n_body_simulation_seq.c
+++ 13/n_body_simulation_seq.c
@@ -22,7 +22,7 @@ typedef struct particle {
 	double mass;
 } particle;
 
-double** compute_force(particle* particles, size_t number_of_particles);
+double** compute_force(particle* particles, double** forces, size_t number_of_particles);
 void apply_force(particle* particles, double** forces, size_t number_of_particles);
 
 int main(int argc, char** argv) {
@@ -47,31 +47,44 @@ int main(int argc, char** argv) {
 		particles[i].mass = rand() % (MASS_MAX - MASS_MIN + 1) + MASS_MIN;
 	}
 
+	double** forces = malloc(number_of_particles * sizeof(double*));
+
 	// Prepare timing measurement
 	double start_time, end_time, elapsed_time;
-
 	start_time = omp_get_wtime();
 
 	// N-body simulation
-
-	for(size_t t = 0; t < number_of_time_steps; t++) {
+	for(size_t t = 1; t < number_of_time_steps; t++) {
 		// Compute forces
-		double** forces = compute_force(particles, number_of_particles);
+		forces = compute_force(particles, forces, number_of_particles);
 
 		// Apply forces
 		apply_force(particles, forces, number_of_particles);
 
-		// Free memory
-		for(size_t i = 0; i < number_of_particles; i++) {
-			free(forces[i]);
+		FILE* fptr = fopen("data.dat", "a");
+		for(size_t i = 0; i < number_of_particles; ++i) {
+			if(fptr == NULL) {
+				printf("Could not open file");
+				return 0;
+			}
+			fprintf(fptr, "%f %f %f\n", particles[i].position[0], particles[i].position[1],
+			        particles[i].position[2]);
 		}
-		free(forces);
+		fprintf(fptr, "\n\n");
+		fclose(fptr);
 	}
 
 	// Stop timing measurement
 	end_time = omp_get_wtime();
 
 	elapsed_time = end_time - start_time;
+
+	// Free memory
+	for(size_t i = 0; i < number_of_particles; i++) {
+		free(forces[i]);
+	}
+	// Print particle positions in desired format
+	free(forces);
 
 	// Free memory
 	free(particles);
@@ -83,8 +96,7 @@ int main(int argc, char** argv) {
 	return EXIT_SUCCESS;
 }
 
-double** compute_force(particle* particles, size_t number_of_particles) {
-	double** forces = malloc(number_of_particles * sizeof(double*));
+double** compute_force(particle* particles, double** forces, size_t number_of_particles) {
 	for(size_t i = 0; i < number_of_particles; i++) {
 		forces[i] = malloc(3 * sizeof(double));
 		double force[3] = { 0 };
@@ -95,6 +107,9 @@ double** compute_force(particle* particles, size_t number_of_particles) {
 					                   particles[j].position[2] - particles[i].position[2] };
 				double r = sqrtl(distance[0] * distance[0] + distance[1] * distance[1] +
 				                 distance[2] * distance[2]);
+				if(r == 0) {
+					r = 0.001;
+				}
 				double f = GRAVITATIONAL_CONSTANT * particles[i].mass * particles[j].mass / (r * r);
 				force[0] += f * distance[0] / r;
 				force[1] += f * distance[1] / r;
